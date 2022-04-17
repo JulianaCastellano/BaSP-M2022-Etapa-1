6ef072426b1716e425b18f59e5adc10574c024ed
juliana castellano <julicastellano98@gmail.com>
2022-04-17 18:36:09 -0300

    BaSP-W05-2022: Variables and Operators

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,27 @@
+console.log("Eje A - Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.");
+
+var pesoViejo = 68
+var aumento = 5
+
+var pesoActual = pesoViejo + aumento
+
+console.log("Resultado" + " " + pesoActual);
+
+console.log("Eje B - Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.");
+
+var suma = "4 + 2 "
+var resultado = "es igual a 6"
+
+var unir = suma + resultado 
+
+console.log(unir);
+
+console.log("Eje C - Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).");
+
+var hi = "Hola "
+var me = "yo soy Juliana"
+
+var rta = hi.length + me.length
+
+console.log("La cantidad de unidades de codigos es " + rta);
+
