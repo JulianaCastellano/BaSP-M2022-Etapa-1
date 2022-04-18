e511e6adfa3f785064dc196a028fe39b09751824
juliana castellano <julicastellano98@gmail.com>
2022-04-17 21:19:22 -0300

    BaSP-W05-2022: Strings

--- Semana-05/exercises/01_variables_and_operators.js
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -1,27 +1,30 @@
-console.log("Eje A - Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.");
+// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+console.log("Excercise 1-a");
 
-var pesoViejo = 68
-var aumento = 5
+var pesoViejo = 68;
+var aumento = 5;
 
-var pesoActual = pesoViejo + aumento
+var pesoActual = pesoViejo + aumento;
 
-console.log("Resultado" + " " + pesoActual);
+console.log("Resoults" + " " + pesoActual);
 
-console.log("Eje B - Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.");
+//Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+console.log("Excercise 1-b");
 
-var suma = "4 + 2 "
-var resultado = "es igual a 6"
+var suma = "4 + 2 ";
+var resultado = "is equal to 6";
 
-var unir = suma + resultado 
+var unir = suma + resultado;
 
 console.log(unir);
 
-console.log("Eje C - Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).");
+//Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+console.log("Excercise 1-c");
 
-var hi = "Hola "
-var me = "yo soy Juliana"
+var hi = "Hi everyone ";
+var me = "I am Juliana";
 
-var rta = hi.length + me.length
+var rta = hi.length + me.length;
 
-console.log("La cantidad de unidades de codigos es " + rta);
+console.log(rta + " letters");
 
