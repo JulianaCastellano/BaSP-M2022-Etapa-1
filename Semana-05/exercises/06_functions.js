3c2c213d18be72a95dd8eda606b33dd11ef48d2c
juliana castellano <julicastellano98@gmail.com>
2022-04-19 19:42:53 -0300

    BaSP-W05-2022: Functions

---
+++ Semana-05/exercises/06_functions.js
@@ -0,0 +1,83 @@
+//Crear una función suma que reciba dos valores numéricos y retorne el resultado. Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador.
+console.log("Excercise 6-a");
+
+function suma(a,b){
+    var resultado;
+    resultado = a + b;
+    console.log(resultado)
+}
+suma(3,5)
+
+//A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado.
+console.log("Excercise 6-b");
+
+function suma(a,b){
+    var resultado;
+    resultado = a + b;
+    if(typeof (a) !== "number" || typeof (b) !== "number" ){
+        alert("ERROR");
+        return resultado = NaN;
+    }else{
+        return console.log(resultado);
+    }
+}
+suma("3",5)
+
+//Crear una función validate integer que reciba un número como parámetro y devuelva verdadero si es un número entero.
+console.log("Excercise 6-c");
+
+function validate(c) {
+    return Number.isInteger(c);
+}
+console.log(validate(5))
+
+//A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros. En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado).
+console.log("Excercise 6-d");
+
+function suma(a,b){
+    var resultado;
+    resultado = a + b;
+    if(typeof (a) !== "number" || typeof (b) !== "number" ){
+        alert("ERROR");
+        return resultado = NaN;
+    }if(validate(a) == false) {
+        alert("ERROR, se redondea " + Math.round(a));
+        return resultado = NaN;
+    }if(validate(b) == false) {
+        alert("ERROR, se redondea " + Math.round(b));
+        return resultado = NaN;
+    } else{
+        return console.log(resultado);
+    }
+}
+suma(3.8,5)
+
+//Convertir la validación del ejercicio 6d) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual.
+console.log("Excercise 6-e");
+
+function sumaNumero(a,b){
+    var resultado;
+    resultado = a + b;
+    if(typeof (a) !== "number" || typeof (b) !== "number" ){
+        alert("ERROR");
+        return resultado = NaN;
+    }else{
+        return console.log(resultado);
+    }
+}
+suma("2",5)
+
+function sumaEnteros(a,b){
+    var resultado;
+    resultado = a + b;
+    if(validate(a) == false) {
+        alert("ERROR, se redondea " + Math.round(a));
+        return resultado = NaN;
+    }if(validate(b) == false) {
+        alert("ERROR, se redondea " + Math.round(b));
+        return resultado = NaN;
+    } else{
+        return console.log(resultado);
+    }
+}
+suma(8.1,5)
